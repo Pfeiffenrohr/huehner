d21b6be3039f207f1c0946c3b3004aa4f7a70bee
Lenovo\richard <richard@richardlechner.de>
2020-04-01 14:15:07 +0200

    Testfolder für arduino

---
+++ Raspberry/Arduino/Test/Test.ino
@@ -0,0 +1,19 @@
+#include <Boards.h>
+#include <Firmata.h>
+
+//Endschalter
+int sensePin3 = 2;
+int wert3 = 0;
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
+
+
