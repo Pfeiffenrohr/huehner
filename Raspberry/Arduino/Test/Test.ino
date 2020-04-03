fc52e3d80b5f491c11c158e0437a2f7361b683ca
Lenovo\richard <richard@richardlechner.de>
2020-04-03 12:08:10 +0200

    Bilder hinzugegügt

--- Raspberry/Arduino/Test/Test.ino
+++ Raspberry/Arduino/Test/Test.ino
@@ -8,12 +8,15 @@ int wert3 = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
-  
+    Serial.begin(9600);  
+    Serial.println("start");
 }
 
 // the loop function runs over and over again forever
 void loop() {
+   wert3 = analogRead(sensePin3);
+   Serial.println(wert3);
+   
 }
 
 
